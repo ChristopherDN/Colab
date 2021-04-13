84cc098e8f1c47b3821194c90883695caea984c3
Josefine <josefine.vihlborg.nielsen@hotmail.com>
2021-04-13 11:35:28 +0200

    Mere er tilføjet til Date-opgave

--- untitled/DateOfTheDay/src/Date.java
+++ untitled/DateOfTheDay/src/Date.java
@@ -2,5 +2,8 @@ public class Date {
   public static void main(String[] args) {
     System.out.println("13-04-2021");
 
+    System.out.println("Tirsdag");
+    System.out.println("Dette er en test");
+
   }
 }
